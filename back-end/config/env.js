c4a4025bfa6efab0f99978d7cc93ba92ca422fc6
Kumbang heracles <kumbangheracles@gmail.com>
2025-06-04 17:23:00 +0700

    minor adjust

--- back-end/config/env.js
+++ back-end/config/env.js
@@ -1,4 +1,5 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
+const dotenv = require("dotenv");
 dotenv.config();
 export const DATABASE_URL = process.env.DATABASE_URL || "";
 export const SECRET = process.env.SECRET || "";
